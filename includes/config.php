31b0e9b7176d8131abba22829eb4c6dde42d52cd
Ashutosh Pandey <159229967+ashutosh7556@users.noreply.github.com>
2026-09-17 16:32:55 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -12,7 +12,6 @@ if ($isLocal) {
     define('DB_NAME', getenv('DB_NAME') ?: 'voting-system');
     define('DB_USER', getenv('DB_USER') ?: 'root');
     define('DB_PASS', getenv('DB_PASS') ?: 'root');
-    define('DB_PORT', getenv('DB_PORT') ?: '5432');
     define('APP_URL', getenv('APP_URL') ?: 'http://localhost/voting_sysem');
     define('APP_ENV', getenv('APP_ENV') ?: 'local');
 } else {
@@ -20,6 +19,7 @@ if ($isLocal) {
     define('DB_NAME', getenv('DB_NAME') ?: 'postgres');
     define('DB_USER', getenv('DB_USER') ?: 'postgres');
     define('DB_PASS', getenv('DB_PASS') ?: 'AshutosH$12');
+    define('DB_PORT', getenv('DB_PORT') ?: '5432');
     define('APP_URL', getenv('APP_URL') ?: 'https://voting-sysem.onrender.com/');
     define('APP_ENV', getenv('APP_ENV') ?: 'production');
 }
